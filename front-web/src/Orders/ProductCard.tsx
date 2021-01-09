506c975240c36cf36a1289e98c46aea766d06b54
Thiago Cedran <thiagocdn@gmail.com>
2021-01-09 14:48:57 -0300

    Finished front-web

--- front-web/src/Orders/ProductCard.tsx
+++ front-web/src/Orders/ProductCard.tsx
@@ -1,22 +1,18 @@
 import { Product } from "./types";
+import { formatPrice } from './helpers';
 
 type Props = {
   product: Product;
+  isSelected: boolean;
+  onSelectProduct: (product: Product) => void;
 }
 
-function formatPrice(price: number) {
-  const formatter = new Intl.NumberFormat('pt-BR', {
-    style: 'currency',
-    currency: 'BRL',
-    minimumFractionDigits: 2,
-  });
-
-  return formatter.format(price);
-}
-
-function ProductsCard ({ product }: Props) {
+function ProductsCard ({ product, onSelectProduct, isSelected }: Props) {
   return (
-    <div className="order-card-container">
+    <div
+      className={`order-card-container ${isSelected && 'selected'}`}
+      onClick={() => onSelectProduct(product)}
+    >
       <h3 className="order-card-title">{product.name}</h3>
       <img className="order-card-image" src={product.imageUri} alt={product.name}/>
       <h3 className="order-card-price">
--- front-web/src/Orders/types.ts
+++ front-web/src/Orders/types.ts
@@ -4,10 +4,19 @@ export type Product = {
   price: number;
   description: string;
   imageUri: string;
-}
+};
 
 export type OrderLocationData = {
   latitude: number;
   longitude: number;
   address: string;
-}
+};
+
+type ProductId = {
+  id: number;
+};
+
+export type OrderPayload = {
+  products: ProductId[];
+} & OrderLocationData;
+
